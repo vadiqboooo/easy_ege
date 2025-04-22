7ebfe88b2aa629175d3504ad3cb7708e94dd3cea
vadiqboooo <vadiqbozhko@gmail.com>
2025-04-22 13:25:10 +0800

    update package and viteconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    chunkSizeWarningLimit: 1600,
-    commonjsOptions: {
-      include: [/node_modules/, /path-to-your-custom-modules/]
-    }
-  }
+  plugins: [react(), tailwindcss()]
 })
