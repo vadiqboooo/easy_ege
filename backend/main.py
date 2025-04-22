1032063cd6ee3d68cd9f01d173a8e39822bee917
vadiqboooo <vadiqbozhko@gmail.com>
2025-04-22 20:10:47 +0800

    fix main.py

--- backend/main.py
+++ backend/main.py
@@ -339,4 +339,4 @@ async def startup_event():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", posrt=8000, reload=True)
